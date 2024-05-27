a28e0487183d120ac7109215330491fcb3995fbe
andreprr <efsa.andre@gmail.com>
2024-05-27 22:06:25 +0700

    add: about

---
+++ resources/views/client/components/footer.blade.php
@@ -0,0 +1,28 @@
+<footer class="footer" id="contact">
+    <div class="footer-wrapper">
+        <h1><span>Sekretariat</span> BEM Indonesia Mandiri</h1>
+        <div class="footer-foot row">
+            <div class="footer-maps col">
+                <p>Jl. Belitung No.7, Merdeka, Kec. Sumur Bandung, Kota Bandung, Jawa Barat 40113</p>
+                <iframe width="200" height="200" id="gmap_canvas"
+                    src="https://maps.google.com/maps?q=perguruan%20tinggi%20indonesia%20mandiri&t=&z=10&ie=UTF8&iwloc=&output=embed"
+                    frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+            </div>
+
+
+            <div class="footer-social col">
+                <h1>Sosial Media</h1>
+                <a href="#"><i class="bi-instagram"></i></a>
+                <a href="#"><i class="bi-youtube"></i></a>
+                <a href="#"><i class="bi-twitter"></i></a>
+                <a href="#"><i class="bi-facebook"></i></a>
+                <h1>Kontak</h1>
+                <div class="contact-social">
+                    <a href="#"><i class="bi-envelope"></i></a>
+                    <a href="#"><i class="bi-telephone"></i></a>
+                </div>
+            </div>
+        </div>
+        <p>&copy; Copyright | &nbsp;<a href="https://www.instagram.com/bem.imandiri/" target="_blank">bem.imandiri</a>.</p>
+    </div>
+</footer>
--- resources/views/client/components/navbar.blade.php
+++ resources/views/client/components/navbar.blade.php
@@ -8,8 +8,8 @@
         <input type="radio" name="slider" id="close-btn">
         <ul class="nav-links">
             <label for="close-btn" class="btn close-btn"><i class="fas fa-times"></i></label>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">About</a></li>
+            <li><a href="/">Home</a></li>
+            <li><a href="{{ route('about') }}">About</a></li>
             <li>
                 <a href="#" class="desktop-item">Departemen Menu</a>
                 <input type="checkbox" id="showDrop">
