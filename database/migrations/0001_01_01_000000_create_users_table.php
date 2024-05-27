4af9e97ecc910aabcf6c0c109fa4b95193cf70a3
wali98-rgb <wyyuddin@gmail.com>
2024-05-27 12:56:22 +0700

    add: configure database

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,10 +13,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('photo')->nullable();
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('gender', ['0', '1']);
+            $table->enum('role', ['guest', 'admin', 'superadmin']);
             $table->rememberToken();
             $table->timestamps();
         });
